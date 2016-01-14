d897c374e46cd693431c314ca0ff76314ef16c4f
Christopher Trott <chris@twocentstudios.com>
2016-01-14 14:19:56 +0900

    UserController

--- UserListValueTypes/UserController.swift
+++ UserListValueTypes/UserController.swift
@@ -4,3 +4,15 @@
 //
 
 import Foundation
+import LoremIpsum
+
+class UserController {
+    func fetchRandomUsers(count: Int = 100) -> Array<User> {
+        return [0...count].map { i in
+            let name = LoremIpsum.name()
+            let avatarURL = LoremIpsum.URLForPlaceholderImageFromService(.Hhhhold, withSize:CGSize(width: 96, height:96)).URLByAppendingPathComponent("jpg?test=\(i)")
+            let user = User(name: name, avatarURL: avatarURL)
+            return user
+        }
+    }
+}
