0c483f53db498c3c7992784d0254f517c0807cae
Christopher Trott <chris@twocentstudios.com>
2016-01-14 14:19:49 +0900

    Bridging Header

---
+++ UserListValueTypes/UserListValueTypes-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Created by Christopher Trott on 1/14/16.
+//  Copyright © 2016 twocentstudios. All rights reserved.
+//
+
+#ifndef UserListValueTypes_Bridging_Header_h
+#define UserListValueTypes_Bridging_Header_h
+
+#import <LoremIpsum/LoremIpsum.h>
+
+#endif /* UserListValueTypes_Bridging_Header_h */
