53fa3b809512f72a6b4eccd2c9237bb66c8f13f0
Christopher Trott <chris@twocentstudios.com>
2016-01-15 21:29:13 +0900

    Formalize the RAC

--- UserListValueTypes/UsersViewModel.swift
+++ UserListValueTypes/UsersViewModel.swift
@@ -9,25 +9,29 @@ import ReactiveCocoa
 class UsersViewModel {
     var userViewModels: [UserViewModel]? = nil
     
-    private let fetchUserViewModelsAction: Action<Void, [UserViewModel], NoError>
-    private let fetchUserViewModelImageProducer: UserViewModel -> SignalProducer<UserViewModel, NoError>
+    private let userController: UserController
+    private let imageController: ImageController
     
+    private let fetchUserViewModelsAction: Action<Void, [UserViewModel], NoError>
+
     let reloadSignal: Signal<Void, NoError>
     private let reloadObserver: Observer<Void, NoError>
     
     let reloadIndexPathsSignal: Signal<[NSIndexPath], NoError>
     private let reloadIndexPathsObserver: Observer<[NSIndexPath], NoError>
     
-    private let viewModelsQueue = QueueScheduler(qos: QOS_CLASS_USER_INITIATED, name: "com.twocentstudios.UserList.viewmodels")
+    let viewModelsQueue = QueueScheduler(qos: QOS_CLASS_USER_INITIATED, name: "com.twocentstudios.UserList.viewmodels")
+    let workQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.work")
+    let imageQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.image")
     
     init(userController: UserController, imageController: ImageController) {
+        self.userController = userController
+        self.imageController = imageController
+        
         (reloadSignal, reloadObserver) = Signal<Void, NoError>.pipe()
         (reloadIndexPathsSignal, reloadIndexPathsObserver) = Signal<[NSIndexPath], NoError>.pipe()
-
-        let workQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.work")
-        let imageQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.image")
-
-        fetchUserViewModelsAction = Action { _ in
+        
+        fetchUserViewModelsAction = Action { [unowned workQueue] _ in
             return SignalProducer(value: 100)
                 .observeOn(workQueue)
                 .flatMap(.Latest, transform: userController.fetchRandomUsersProducer)
@@ -38,19 +42,11 @@ class UsersViewModel {
                 }
         }
         
-        fetchUserViewModelImageProducer = { userViewModel in
-            return SignalProducer(value: userViewModel.avatarImageData.input)
-                .observeOn(imageQueue)
-                .flatMap(.Latest, transform: imageController.loadImageDataProducer)
-                .map(userViewModel.withData)
-                .flatMapError { SignalProducer(value: userViewModel.withError($0)) }
-        }
-        
         fetchUserViewModelsAction.values
             .observeOn(viewModelsQueue)
-            .observeNext { [weak self] userViewModels in
-                self?.userViewModels = userViewModels
-                self?.reloadObserver.sendNext()
+            .observeNext { [unowned self] userViewModels in
+                self.userViewModels = userViewModels
+                self.reloadObserver.sendNext()
             }
     }
     
@@ -61,25 +57,64 @@ class UsersViewModel {
     func activateIndexPath(indexPath: NSIndexPath) {
         guard let userViewModel = userViewModels?[indexPath.row] else { return }
         if userViewModel.shouldFetchAvatarImage() {
-            fetchUserViewModelImageProducer(userViewModel)
-                .observeOn(viewModelsQueue)
-                .startWithNext { [weak self] userViewModel in
-                    guard let userViewModels = self?.userViewModels else { return }
-                    let newUserViewModelsAndIndicies = userViewModels.enumerate().map { (index, oldUserViewModel) -> (Int?, UserViewModel) in
-                        if oldUserViewModel =~= userViewModel {
-                            return (index, userViewModel)
-                        }
-                        return (nil, oldUserViewModel)
-                    }
-                    let newUserViewModels = newUserViewModelsAndIndicies.map { $0.1 }
-                    let changedIndicies = newUserViewModelsAndIndicies.map { $0.0 }.flatMap { $0 }
-                    let changedIndexPaths = changedIndicies.map { NSIndexPath(forRow: $0, inSection: 0) }
-                    
-                    self?.userViewModels = newUserViewModels
-                    self?.reloadIndexPathsObserver.sendNext(changedIndexPaths)
-                }
+            self.loadUserViewModelImageProducer(userViewModel).start()
         }
     }
     
+    private func loadUserViewModelImageProducer(userViewModel: UserViewModel) -> SignalProducer<(UserViewModel, [UserViewModel], [NSIndexPath]), NoError> {
+        return SignalProducer<UserViewModel, NoError>(value: userViewModel)
+            .observeOn(workQueue)
+            .flatMap(FlattenStrategy.Latest, transform: self.loadingImageUserViewModelImageProducer)
+            .observeOn(viewModelsQueue)
+            .flatMap(.Latest, transform: { [unowned self] viewModel -> SignalProducer<(UserViewModel, [UserViewModel], [NSIndexPath]), NoError> in
+                guard let userViewModels = self.userViewModels else { return .empty }
+                return self.replaceUserViewModelProducer(viewModel, userViewModels: userViewModels)
+            })
+            .on(next: { [unowned self] (userViewModel, userViewModels, changedIndexPaths) in
+                self.userViewModels = userViewModels
+                self.reloadIndexPathsObserver.sendNext(changedIndexPaths)
+            })
+            .observeOn(imageQueue)
+            .map { (viewModel: UserViewModel, _, _) in return viewModel }
+            .flatMap(.Latest, transform: self.fetchUserViewModelImageProducer)
+            .observeOn(viewModelsQueue)
+            .flatMap(.Latest, transform: { [unowned self] (viewModel: UserViewModel) -> SignalProducer<(UserViewModel, [UserViewModel], [NSIndexPath]), NoError> in
+                guard let userViewModels = self.userViewModels else { return .empty }
+                return self.replaceUserViewModelProducer(viewModel, userViewModels: userViewModels)
+            })
+            .on(next: { [unowned self] (_, userViewModels, changedIndexPaths) in
+                self.userViewModels = userViewModels
+                self.reloadIndexPathsObserver.sendNext(changedIndexPaths)
+            })
+    }
     
+    private func fetchUserViewModelImageProducer(userViewModel: UserViewModel) -> SignalProducer<UserViewModel, NoError> {
+        return SignalProducer(value: userViewModel.avatarImageData.input)
+            .observeOn(self.imageQueue)
+            .flatMap(.Latest, transform: imageController.loadImageDataProducer)
+            .map(userViewModel.withData)
+            .flatMapError { SignalProducer(value: userViewModel.withError($0)) }
+    }
+    
+    private func loadingImageUserViewModelImageProducer(userViewModel: UserViewModel) -> SignalProducer<UserViewModel, NoError> {
+        return SignalProducer(value: userViewModel)
+            .map { $0.withLoading(0) }
+    }
+    
+    private func replaceUserViewModelProducer(userViewModel: UserViewModel, userViewModels: [UserViewModel]) -> SignalProducer<(UserViewModel, [UserViewModel], [NSIndexPath]), NoError> {
+        return SignalProducer(value: (userViewModel, userViewModels))
+            .map { (userViewModel, userViewModels) in
+                let newUserViewModelsAndIndicies = userViewModels.enumerate().map { (index, oldUserViewModel) -> (Int?, UserViewModel) in
+                    if oldUserViewModel =~= userViewModel {
+                        return (index, userViewModel)
+                    }
+                    return (nil, oldUserViewModel)
+                }
+                let newUserViewModels = newUserViewModelsAndIndicies.map { $0.1 }
+                let changedIndicies = newUserViewModelsAndIndicies.map { $0.0 }.flatMap { $0 }
+                let changedIndexPaths = changedIndicies.map { NSIndexPath(forRow: $0, inSection: 0) }
+                return (userViewModel, newUserViewModels, changedIndexPaths)
+        }
+    }
+
 }
