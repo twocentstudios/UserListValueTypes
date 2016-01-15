3e1dc8b9c81a7c3f5362f025994e49b2d84d5e6c
Christopher Trott <chris@twocentstudios.com>
2016-01-15 17:19:43 +0900

    Use Producer instead of Action

--- UserListValueTypes/UsersViewModel.swift
+++ UserListValueTypes/UsersViewModel.swift
@@ -10,7 +10,7 @@ class UsersViewModel {
     var userViewModels: [UserViewModel]? = nil
     
     private let fetchUserViewModelsAction: Action<Void, [UserViewModel], NoError>
-    private let fetchUserViewModelImageAction: Action<UserViewModel, UserViewModel, NoError>
+    private let fetchUserViewModelImageProducer: UserViewModel -> SignalProducer<UserViewModel, NoError>
     
     let reloadSignal: Signal<Void, NoError>
     private let reloadObserver: Observer<Void, NoError>
@@ -18,13 +18,14 @@ class UsersViewModel {
     let reloadIndexPathsSignal: Signal<[NSIndexPath], NoError>
     private let reloadIndexPathsObserver: Observer<[NSIndexPath], NoError>
     
+    private let viewModelsQueue = QueueScheduler(qos: QOS_CLASS_USER_INITIATED, name: "com.twocentstudios.UserList.viewmodels")
+    
     init(userController: UserController, imageController: ImageController) {
         (reloadSignal, reloadObserver) = Signal<Void, NoError>.pipe()
         (reloadIndexPathsSignal, reloadIndexPathsObserver) = Signal<[NSIndexPath], NoError>.pipe()
 
         let workQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.work")
         let imageQueue = QueueScheduler(qos: QOS_CLASS_DEFAULT, name: "com.twocentstudios.UserList.image")
-        let viewModelsQueue = QueueScheduler(qos: QOS_CLASS_USER_INITIATED, name: "com.twocentstudios.UserList.viewmodels")
 
         fetchUserViewModelsAction = Action { _ in
             return SignalProducer { observer, disposable in
@@ -40,7 +41,7 @@ class UsersViewModel {
             .startOn(workQueue)
         }
         
-        fetchUserViewModelImageAction = Action { userViewModel in
+        fetchUserViewModelImageProducer = { userViewModel in
             return SignalProducer { observer, disposable in
                 let avatarURL = userViewModel.user.avatarURL
                 let data = imageController.loadImageData(avatarURL)
@@ -57,24 +58,6 @@ class UsersViewModel {
                 self?.userViewModels = userViewModels
                 self?.reloadObserver.sendNext()
             }
-        
-        fetchUserViewModelImageAction.values
-            .observeOn(viewModelsQueue)
-            .observeNext { [weak self] userViewModel in
-                guard let userViewModels = self?.userViewModels else { return }
-                let newUserViewModelsAndIndicies = userViewModels.enumerate().map { (index, oldUserViewModel) -> (Int?, UserViewModel) in
-                    if oldUserViewModel =~= userViewModel {
-                        return (index, userViewModel)
-                    }
-                    return (nil, oldUserViewModel)
-                }
-                let newUserViewModels = newUserViewModelsAndIndicies.map { $0.1 }
-                let changedIndicies = newUserViewModelsAndIndicies.map { $0.0 }.flatMap { $0 }
-                let changedIndexPaths = changedIndicies.map { NSIndexPath(forRow: $0, inSection: 0) }
-                
-                self?.userViewModels = newUserViewModels
-                self?.reloadIndexPathsObserver.sendNext(changedIndexPaths)
-            }
     }
     
     func fetchUserViewModels() {
@@ -83,8 +66,24 @@ class UsersViewModel {
     
     func activateIndexPath(indexPath: NSIndexPath) {
         guard let userViewModel = userViewModels?[indexPath.row] else { return }
-        if !userViewModel.shouldFetchImage() { return }
-        
-        fetchUserViewModelImageAction.apply(userViewModel).start()
+        if userViewModel.shouldFetchImage() {
+            fetchUserViewModelImageProducer(userViewModel)
+                .observeOn(viewModelsQueue)
+                .startWithNext { [weak self] userViewModel in
+                    guard let userViewModels = self?.userViewModels else { return }
+                    let newUserViewModelsAndIndicies = userViewModels.enumerate().map { (index, oldUserViewModel) -> (Int?, UserViewModel) in
+                        if oldUserViewModel =~= userViewModel {
+                            return (index, userViewModel)
+                        }
+                        return (nil, oldUserViewModel)
+                    }
+                    let newUserViewModels = newUserViewModelsAndIndicies.map { $0.1 }
+                    let changedIndicies = newUserViewModelsAndIndicies.map { $0.0 }.flatMap { $0 }
+                    let changedIndexPaths = changedIndicies.map { NSIndexPath(forRow: $0, inSection: 0) }
+                    
+                    self?.userViewModels = newUserViewModels
+                    self?.reloadIndexPathsObserver.sendNext(changedIndexPaths)
+                }
+        }
     }
 }
