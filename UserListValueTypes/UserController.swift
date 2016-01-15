c44733e1fcf83b85a9e788eb23f4a80d92d135e6
Christopher Trott <chris@twocentstudios.com>
2016-01-16 00:59:53 +0900

    UserController docs

--- UserListValueTypes/UserController.swift
+++ UserListValueTypes/UserController.swift
@@ -7,14 +7,23 @@ import Foundation
 import ReactiveCocoa
 import LoremIpsum
 
+/// UserController is the data source for User objects. The underlying data source could be disk, network, etc.
+///
+/// In this architecture, Controller objects obscure the data source from the ViewModel layer and provide
+/// a consistent interface. Controller objects cannot access members of the View or ViewModel layers.
+///
+/// UserController is a reference-type because it may implement its own layer of caching.
 class UserController {
+    /// Asynchronous fetch.
     func fetchRandomUsersProducer(count: Int = 100) -> SignalProducer<[User], NoError> {
         return SignalProducer { observer, disposable in
             observer.sendNext(UserController.fetchRandomUsers(count))
             observer.sendCompleted()
         }
+        // .delay(2, onScheduler: QueueScheduler()) /// Simulate a network delay.
     }
     
+    /// Generate the specified number of random User objects.
     private static func fetchRandomUsers(count: Int) -> [User] {
         return (0..<count).map { i in
             let name = LoremIpsum.name()
