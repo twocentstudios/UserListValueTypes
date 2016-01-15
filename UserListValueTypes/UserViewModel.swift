52d78009d59740bbd0cf2312bfcd32f8ce5ae9d4
Christopher Trott <chris@twocentstudios.com>
2016-01-16 00:37:29 +0900

    UserViewModel docs

--- UserListValueTypes/UserViewModel.swift
+++ UserListValueTypes/UserViewModel.swift
@@ -5,15 +5,24 @@
 
 import Foundation
 
+/// UserViewModel is a value type that contains the static data prepared for consumption by the view layer.
+/// The avatarImageData field represents an external image resource in various states (empty, loading, loaded, errored).
+/// As a value-type, in order to change states, a completely new UserViewModel must be created.
 struct UserViewModel {
+    
+    /// The underlying model object that this view model represents.
+    /// This attribute is considered invisible to the View layer of the application.
     let user: User
+    
     let name: String
     let avatarImageData: AsyncResource<NSURL, NSData>
     
+    /// Convenience initializer for creating a view model from a User model and no other data.
     init(user: User) {
         self.init(user: user, avatarImageData: AsyncResource(input: user.avatarURL, output: AsyncResourceState.Empty))
     }
     
+    /// Designated initalizer.
     init(user: User, avatarImageData: AsyncResource<NSURL, NSData>) {
         self.user = user
         self.name = user.name
@@ -24,6 +33,7 @@ struct UserViewModel {
         return avatarImageData.shouldFetch()
     }
     
+    /// TODO: These mutators would probably be better as lenses.
     func withData(data: NSData) -> UserViewModel {
         let resource = self.avatarImageData.withOutput(.Loaded(data))
         let loadedUserViewModel = UserViewModel(user: self.user, avatarImageData: resource)
@@ -43,6 +53,7 @@ struct UserViewModel {
     }
 }
 
+/// We consider UserViewModels to be of equal identity if their underlying User models are the equal.
 extension UserViewModel: Identifiable {}
 func =~=(lhs: UserViewModel, rhs: UserViewModel) -> Bool {
     return lhs.user == rhs.user
