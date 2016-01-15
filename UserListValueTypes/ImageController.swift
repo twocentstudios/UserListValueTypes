15ee70fcb673305f135ee3941990bd8baaace1dc
Christopher Trott <chris@twocentstudios.com>
2016-01-16 01:27:47 +0900

    ImageController docs

--- UserListValueTypes/ImageController.swift
+++ UserListValueTypes/ImageController.swift
@@ -6,22 +6,30 @@
 import Foundation
 import ReactiveCocoa
 
+/// ImageController is the data source for image data.
+///
+/// We're specifically not vending UIImage from this class as an experiment in order to keep UIKit 
+/// out of our ViewModel and Model layers.
+///
+/// This implementation is extremely bare. In theory, a layer of caching could sit below this class
+/// as well as proper networking.
 class ImageController {
-    func loadImageDataProducer(url: NSURL) -> SignalProducer<NSData, NSError> {
+    func loadImageDataProducer(URL: NSURL) -> SignalProducer<NSData, NSError> {
         return SignalProducer { observer, disposable in
             let simulateRandomFailure = false // (arc4random_uniform(5) == 0)
-            if let image = ImageController.loadImageData(url) where simulateRandomFailure == false {
+            if let image = ImageController.loadImageData(URL) where simulateRandomFailure == false {
                 observer.sendNext(image)
                 observer.sendCompleted()
             } else {
-                observer.sendFailed(NSError(domain: "", code: 0, userInfo: nil))
+                observer.sendFailed(NSError(domain: "com.twocentstudios.UserList", code: 0, userInfo: [NSLocalizedDescriptionKey: "Image data failed to load."]))
             }
         }
     }
     
-    private static func loadImageData(url: NSURL) -> NSData? {
-        // Don't try this at home
-        guard let data = NSData(contentsOfURL: url) else { return nil }
+    /// Synchronously convert an image URL to data.
+    private static func loadImageData(URL: NSURL) -> NSData? {
+        // Don't try this at home.
+        guard let data = NSData(contentsOfURL: URL) else { return nil }
         return data
     }
 }
