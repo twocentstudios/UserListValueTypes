78257f2c69f2e178a4572dcf865b949a0d56e857
Christopher Trott <chris@twocentstudios.com>
2016-01-16 16:35:01 +0900

    UserCell docs

--- UserListValueTypes/UserCell.swift
+++ UserListValueTypes/UserCell.swift
@@ -5,16 +5,22 @@
 
 import UIKit
 
-class UserCell: UITableViewCell {
+/// UserCell is intended to be simple in scope.
+final class UserCell: UITableViewCell {
     static let reuseIdentifier = "UserCell"
     
+    /// The built in imageView is quirky. We'll use our own.
     let avatarImageView: UIImageView
+    
+    /// activityIndicatorView is annoying but helps visually convey the state changes.
     let activityIndicatorView: UIActivityIndicatorView
 
+    /// Do all cell configuration in userViewModel. This will be set on every state change and reuse.
     var userViewModel: UserViewModel? {
         didSet {
             self.textLabel?.text = userViewModel?.name ?? ""
             
+            /// Only set the image for loaded state.
             self.avatarImageView.image = userViewModel.flatMap { viewModel in
                 switch viewModel.avatarImageData.output {
                 case .Empty: return nil
@@ -24,6 +30,7 @@ class UserCell: UITableViewCell {
                 }
             }
             
+            /// Background color conveys state changes when not loaded.
             self.avatarImageView.backgroundColor = userViewModel.flatMap { viewModel in
                 switch viewModel.avatarImageData.output {
                 case .Empty: return UIColor(patternImage: UIImage(named: "placeholder")!)
@@ -33,6 +40,7 @@ class UserCell: UITableViewCell {
                 }
             }
             
+            /// Show the activityIndicatorView when loading.
             if let viewModel = userViewModel {
                 switch viewModel.avatarImageData.output {
                 case .Loading: activityIndicatorView.startAnimating()
@@ -60,6 +68,8 @@ class UserCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        /// Don't try this at home.
         let height = CGRectGetHeight(self.bounds)
         self.avatarImageView.frame = CGRect(x: 0, y: 0, width: height, height: height)
         self.textLabel!.frame = CGRect(x: height, y: 0, width: 200, height: height)
